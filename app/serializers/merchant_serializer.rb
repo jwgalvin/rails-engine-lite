f99a8f8bcc7e540dc5ca432a624b7bb201f6bffb
Joseph W Galvin <jwgalvin@gmail.com>
2022-03-28 18:14:56 -0600

    Feat: Merchant find 1 and all working

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,6 +1,7 @@
 class MerchantSerializer
   include JSONAPI::Serializer
+
   set_type :merchant
   set_id :id
-  attributes :item
+  attributes :name
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
     name {Faker::Appliance.equipment}
-    description {Faker::Movies::Starwars.quote}
-    unit_price {Faker::Number(digits: 4)}
+    description {Faker::Movies::StarWars.quote}
+    unit_price {Faker::Number.number(digits: 4)}
     merchant
   end
 end

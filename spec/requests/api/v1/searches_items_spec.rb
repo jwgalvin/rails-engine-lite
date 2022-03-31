1b59c3729b0b5d737081949a379ed28f0ad2f1f9
Joseph W Galvin <jwgalvin@gmail.com>
2022-03-31 16:23:10 -0600

    seperated and cleaned up search_specs

--- spec/requests/api/v1/searches_items_spec.rb
+++ spec/requests/api/v1/searches_items_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Finds all the merchants' do
+describe 'Finds Item(s)' do
   it 'searches for and returns just 1 item' do
     merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
     merchant2 = Merchant.create!(name: "MarksEmporium")
@@ -31,17 +31,16 @@ describe 'Finds all the merchants' do
     get '/api/v1/items/find_all?name=a t'
 
     items = JSON.parse(response.body, symbolize_names: true)
-    #binding.pry
     expect(response).to be_successful
     expect(items[:data].count).to eq(2)
 
-      items[:data].each do |item|
-        #binding.pry
-        expect(item[:attributes][:name]).to be_a(String)
-        expect(item[:attributes]).to have_key(:description)
-        expect(item[:attributes]).to have_key(:unit_price)
-        expect(item[:attributes]).to have_key(:merchant_id)
-      end
+    items[:data].each do |item|
+      #binding.pry
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes]).to have_key(:merchant_id)
+    end
     end
 
   it 'searches for a non-existent item' do
@@ -53,42 +52,17 @@ describe 'Finds all the merchants' do
     expect(items[:data].count).to eq(0)
   end
 
-  it "searches ping the merchants api and returns 1" do
-    merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
-    merchant2 = Merchant.create!(name: "Marks Emporium")
-    merchant3 = Merchant.create!(name: "Perrenial distraction Emporium")
-
-    get '/api/v1/merchants/find?name=In'
-    merchant = JSON.parse(response.body, symbolize_names: true)
-    expect(response).to be_successful
-    #binding.pry
-    expect(merchant[:data].count).to eq(3)
-    expect(merchant[:data][:attributes][:name]).to be_a(String)
-    expect(merchant[:data]).to have_key(:id)
-  end
-
-  it "searches ping the merchants api and returns 0" do
-    merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
-    merchant2 = Merchant.create!(name: "Marks Emporium")
-    merchant3 = Merchant.create!(name: "Perrenial distraction Emporium")
-
-    get '/api/v1/merchants/find?name=park'
-    merchant = JSON.parse(response.body, symbolize_names: true)
-    #binding.pry
-    expect(response).to_not be_successful
-    expect(merchant[:data][:message]).to eq("No matching merchant")
-  end
-
-  it 'searches for and returns just 1 item' do
+  it 'searches items by price(min) for and returns just 1 item' do
     merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
     item1 = merchant1.items.create!(description: "sumtin", name: "a ting", unit_price: 230)
     item2 = merchant1.items.create!(description: "atin", name: "ang", unit_price: 41)
     item3 = merchant1.items.create!(description: "mortin", name: "a ti", unit_price: 420)
     item4 = merchant1.items.create!(description: "stin", name: "ing", unit_price: 40)
 
-    get '/api/v1/items/find_all?minimum=0'
+    get '/api/v1/merchants/find_all?minimum=0'
     item_parsed = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
     expect(response).to_not be_successful
   end
+
+
 end
---
+++ spec/requests/api/v1/searches_merchants_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe 'Finds the merchants:::' do
+  it 'searches for and returns all with search merchant' do
+    merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
+    merchant2 = Merchant.create!(name: "MarksEmporium")
+    merchant3 = Merchant.create!(name: "Perrenial distraction Emporium")
+    merchant4 = Merchant.create!(name: "Marta Emporium")
+    # item1 = merchant1.items.create!(description: "sumtin", name: "a ting", unit_price: 230)
+    # item2 = merchant1.items.create!(description: "atin", name: "ang", unit_price: 41)
+    # item3 = merchant1.items.create!(description: "mortin", name: "a ti", unit_price: 420)
+    # item4 = merchant2.items.create!(description: "stin", name: "ing", unit_price: 40)
+
+    get '/api/v1/merchants/find_all?name=emp'
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    #binding.pry
+    expect(merchants[:data].count).to eq(4)
+    merchants[:data].each do |merchant|
+      expect(merchant[:attributes][:name]).to be_a(String)
+      expect(merchant[:attributes]).to have_key(:name)
+    end
+  end
+
+  it 'sad path for a non-existent merchant' do
+    merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
+    merchant2 = Merchant.create!(name: "Marks Emporium")
+    merchant3 = Merchant.create!(name: "Perrenial distraction Emporium")
+    merchant4 = Merchant.create!(name: "Notta Emporium")
+    get '/api/v1/merchants/find_all?name='
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to_not be_successful
+    #expect(merchant[:data].count).to eq(0)
+  end
+
+  it "searches ping the merchants api and returns 1" do
+    merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
+    merchant2 = Merchant.create!(name: "Marks Emporium")
+    merchant3 = Merchant.create!(name: "Perrenial distraction Emporium")
+
+    get '/api/v1/merchants/find?name=In'
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+
+    expect(merchant[:data].count).to eq(3) #ID, attributes, type
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+    expect(merchant[:data]).to have_key(:id)
+  end
+
+  it "sad path: look for merchants that doesn't exist, returns 0" do
+    merchant1 = Merchant.create!(name: "Inigo's Revenge Emporium")
+    merchant2 = Merchant.create!(name: "Marks Emporium")
+    merchant3 = Merchant.create!(name: "Perrenial distraction Emporium")
+
+    get '/api/v1/merchants/find?name=park'
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    #binding.pry
+    expect(response).to_not be_successful
+    expect(merchant[:data][:message]).to eq("No matching merchant")
+  end
+
+
+end
